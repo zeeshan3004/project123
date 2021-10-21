72e795afb62e04901b3b97f3ab012aab5e147612
zeeshan3004 <80521902+zeeshan3004@users.noreply.github.com>
2021-10-21 16:56:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+leftwristx=0;
+rightwristx=0;
+difference=0;
+
+
+
 function setup(){
     canvas=createCanvas(550,550);
     canvas.position(560,150);
@@ -13,6 +19,10 @@ function setup(){
 
 function draw(){
     background("#808080");
+    fill("#FFFFFF");
+    text("Zeeshan",50,400);
+    textSize(difference);
+    document.getElementById("font_size").innerHTML="Font Size Of The Text: "+difference+"px";
 }
 
 function modolLoaded(){
@@ -22,5 +32,9 @@ function modolLoaded(){
 function gotPoses(results){
   if (results.length>0){
       console.log(results);
+      leftwristx=results[0].pose.leftWrist.x;
+      rightwristx=results[0].pose.rightWrist.x;
+      difference=floor(leftwristx-rightwristx);
+      console.log("leftwristx= "+leftwristx+"rightwristx= "+rightwristx+"difference= "+difference);
   }
 }
